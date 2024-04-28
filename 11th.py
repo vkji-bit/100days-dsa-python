72d2f41262522a3eae531a77ba3b1c8dc53b67d6
Vineet Kumar Gupta <139715960+vkji-bit@users.noreply.github.com>
2024-04-28 20:58:30 +0530

    Add files via upload

---
+++ 11th.py
@@ -0,0 +1,9 @@
+# Multiply Large Numbers represented as Strings
+def add(digit1,digit2):
+    sum=int(digit1)+int(digit2)
+    return sum
+# taking input as string 
+num1=input("Enter first number: ")
+num2=input("Enter first number: ")
+
+print("Result:",add(num1,num2))
